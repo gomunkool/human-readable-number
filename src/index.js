9d5e9d13b3a46b8dc7f84aed11727a8d414d212c
gomunkool <75207234+gomunkool@users.noreply.github.com>
2021-08-09 00:23:11 +0300

    fix bag

--- src/index.js
+++ src/index.js
@@ -1,48 +1,45 @@
 module.exports = function toReadable(number) {
+    // function toReadable(number) {
     const ones = [
         "",
-        " one",
-        " two",
-        " three",
-        " four",
-        " five",
-        " six",
-        " seven",
-        " eight",
-        " nine",
-        " ten",
-        " eleven",
-        " twelve",
-        " thirteen",
-        " fourteen",
-        " fifteen",
-        " sixteen",
-        " seventeen",
-        " eighteen",
-        " nineteen",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
     ];
     const tens = [
         "",
         "",
-        " twenty",
-        " thirty",
-        " forty",
-        " fifty",
-        " sixty",
-        " seventy",
-        " eighty",
-        " ninety",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
     ];
     let res;
     const hundred = " hundred";
-    arrNum = [];
+
     const numberArr = String(number).split("");
-    numberArr.map((item) => {
-        item = Number(item);
-        arrNum.push(item);
-    });
+    let arrNum = numberArr.map((item) => Number(item));
     arrNumLength = arrNum.length;
-    console.log(arrNum);
 
     if (number === 0) {
         res = "zero";
@@ -51,7 +48,7 @@ module.exports = function toReadable(number) {
         res = ones[number];
     }
     if (number >= 20) {
-        res = `${tens[arrNum[0]]}${ones[arrNum[1]]}`;
+        res = `${tens[arrNum[0]]} ${ones[arrNum[1]]}`;
     }
     if (number >= 100) {
         res = `${ones[arrNum[0]]} hundred`;
@@ -59,16 +56,18 @@ module.exports = function toReadable(number) {
             String(arrNum[1]) + String(arrNum[2]) < 20 &&
             String(arrNum[1]) + String(arrNum[2]) > 0
         ) {
-            res = res + `${ones[number - arrNum[0] * 100]}`;
+            res = res + ` ${ones[number - arrNum[0] * 100]}`;
         }
         if (
             String(arrNum[1]) + String(arrNum[2]) >= 20 &&
             String(arrNum[1]) + String(arrNum[2]) < 100
         ) {
-            res = res + `${tens[arrNum[1]]}${ones[arrNum[2]]}`;
+            res = res + ` ${tens[arrNum[1]]} ${ones[arrNum[2]]}`;
         }
     }
-
-    return res;
+    res = res.split("");
+    if (res[res.length - 1] === " ") {
+        res.splice(-1, 1);
+    }
+    return res.join("");
 };
-console.log(toReadable(0));
